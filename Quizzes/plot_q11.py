bb2b63372ddf6ecb9d8d54322cb1bf2bd098b7d1
Paige Lorson <lorsonp@oregonstate.edu>
2020-05-09 14:00:37 -0700

    q11

--- Quizzes/plot_q11.py
+++ Quizzes/plot_q11.py
@@ -5,29 +5,15 @@ import os
 
 L = 1
 x = np.arange(0,20, 0.001)
-p = L/2*np.exp(-L*abs(x))
+p = L/2*np.exp(-L*x**2)
 fig, ax = matplotlib.pyplot.subplots()
 fig.set_figheight(5)
 fig.set_figwidth(7)
 ax.plot(x, p, 'k:', label='Total entropy')
 print('here')
-# matplotlib.pyplot.text(0, 1040, 'Stiff Conditions', fontsize='large')
-# matplotlib.pyplot.text(0, 725, 'Non-Stiff Conditions', fontsize='large')
-# ax.legend(loc='upper center', fontsize='large')
+
 matplotlib.pyplot.xlabel('x', fontsize='large')
-matplotlib.pyplot.ylabel('p(x)', fontsize='large')
-# matplotlib.pyplot.ylim(top=10)
+matplotlib.pyplot.ylabel('p_F(x)', fontsize='large')
 matplotlib.pyplot.tight_layout()
-# matplotlib.pyplot.tick_params(
-#     axis='x',          # changes apply to the x-axis
-#     which='both',      # both major and minor ticks are affected
-#     bottom=False,      # ticks along the bottom edge are off
-#     top=False,         # ticks along the top edge are off
-#     labelbottom=False) # labels along the bottom edge are off
-# matplotlib.pyplot.tick_params(
-#     axis='y',          # changes apply to the x-axis
-#     which='both',      # both major and minor ticks are affected
-#     bottom=False,      # ticks along the bottom edge are off
-#     top=False,         # ticks along the top edge are off
-#     labelleft=False) # labels along the bottom edge are off
+
 matplotlib.pyplot.show()
